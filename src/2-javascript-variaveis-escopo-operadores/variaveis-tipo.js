88c2938a32875a4dfe03da8f920a82b1ed399d73
Mateus Barbosa <mateusx@poli.ufrj.br>
2021-05-28 09:41:12 -0300

    [javascript-variaveis-escopo-operadores] criar variaveis-tipo

---
+++ src/2-javascript-variaveis-escopo-operadores/variaveis-tipo.js
@@ -0,0 +1,22 @@
+var boolTrue = true; // {1}
+var num = 1; // {2}
+var numFloat = 1.3; // {3}
+var meuNome = "Mateus"; // {4}
+var nulo = null; // {5}
+var indefinido; // {6}
+var meuArray = [1, 2, 3, 4];
+var meuObjeto = {
+  nome: "Mateus",
+};
+meuNome = "Não é Johnny"; // {7}
+num = 2; // {8}
+numFloat = 2.5; // {9}
+
+console.log("boolTrue", boolTrue);
+console.log("num", num);
+console.log("numFloat", numFloat);
+console.log("meuNome", meuNome);
+console.log("nulo", nulo);
+console.log("indefinido", indefinido);
+console.log("meuArray", meuArray);
+console.log("meuObjeto", meuObjeto);
